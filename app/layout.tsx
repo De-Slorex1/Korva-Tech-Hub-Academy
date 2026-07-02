469be21331a9a7c568e993004d833e6a9d688d0f
De-Slorex1 <jacobsamson2000@gmail.com>
2026-07-02 13:47:05 +0100

    tiktok ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { Analytics } from '@vercel/analytics/next'
 import type { Metadata, Viewport } from 'next'
+import Script from "next/script";
 
 import './globals.css'
 import { Toaster } from "sonner"
@@ -13,7 +14,7 @@ const geistMono = {
 
 export const metadata: Metadata = {
   title: 'Korva Tech Hub Academy',
-  description: 'Created with v0',
+  description: 'Korva Tech Hub Academy helps aspiring software engineers build job-ready skills through hands-on training in software development, UI/UX design, data analysis, AI, cybersecurity, and more.',
   generator: 'v0.app',
   icons: {
     icon: [
@@ -47,11 +48,70 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="en" className={`${geistSans.className} ${geistMono.className}`}>
       <body className="font-sans antialiased">
         {children}
         <Toaster richColors />
         {process.env.NODE_ENV === 'production' && <Analytics />}
+
+        <Script
+            id="tiktok-pixel"
+            strategy="afterInteractive"
+          >
+            {`
+              !function (w, d, t) {
+                w.TiktokAnalyticsObject=t;
+                var ttq=w[t]=w[t]||[];
+                ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"];
+                ttq.setAndDefer=function(t,e){
+                  t[e]=function(){
+                    t.push([e].concat(Array.prototype.slice.call(arguments,0)))
+                  }
+                };
+                for(var i=0;i<ttq.methods.length;i++){
+                  ttq.setAndDefer(ttq,ttq.methods[i]);
+                }
+
+                ttq.instance=function(t){
+                  var e=ttq._i[t]||[];
+                  for(var n=0;n<ttq.methods.length;n++){
+                    ttq.setAndDefer(e,ttq.methods[n]);
+                  }
+                  return e;
+                };
+
+                ttq.load=function(e,n){
+                  var r="https://analytics.tiktok.com/i18n/pixel/events.js",
+                      o=n&&n.partner;
+
+                  ttq._i=ttq._i||{};
+                  ttq._i[e]=[];
+                  ttq._i[e]._u=r;
+
+                  ttq._t=ttq._t||{};
+                  ttq._t[e]=+new Date;
+
+                  ttq._o=ttq._o||{};
+                  ttq._o[e]=n||{};
+
+                  n=document.createElement("script");
+                  n.type="text/javascript";
+                  n.async=true;
+                  n.src=r+"?sdkid="+e+"&lib="+t;
+
+                  var s=document.getElementsByTagName("script")[0];
+                  if (s.parentNode) {
+                    s.parentNode.insertBefore(n, s);
+                  }
+                };
+
+                ttq.load("D935FPBC77U79CKEN2O0");
+                ttq.page();
+
+              }(window, document, "ttq");
+            `}
+          </Script>
+
       </body>
     </html>
   )
